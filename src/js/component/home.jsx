332feae6c83aa734a5ae331755b489f0eb1329c5
elash1312 <lasherin1312@gmail.com>
2022-02-23 23:39:26 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,18 +6,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div class="container-fluid">
+			<h1 className="text-center mt-5">To-Do's</h1>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
+				Made by Erin Lash @{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				💔!
 			</p>
 		</div>
 	);
