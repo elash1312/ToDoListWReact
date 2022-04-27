fb0d05bcd041a92e13680a9040fa7dc86482166e
elash1312 <lasherin1312@gmail.com>
2022-04-27 21:14:41 +0000

    created components for app

---
+++ src/js/component/app.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const App = () => {
+	const [taskName, setTaskName] = useState("");
+	const [taskList, setTaskList] = useState([]);
+
+	const addTask = () => {
+		setTaskList([...taskList, { task: taskName }]);
+		setTaskName("");
+	};
+
+	return (
+		<div className="container">
+			<div className="App">
+				<h1>Todo List</h1>
+				<label>Task</label>
+				<input
+					type="text"
+					id="task"
+					onChange={(e) => {
+						setTaskName(e.target.value);
+					}}
+				/>
+				<button onClick={addTask}>Add to list</button>
+			</div>
+		</div>
+	);
+};
+export default App;
