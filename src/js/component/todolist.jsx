3b3da4f68f561cfcc6f4875f1c72e73b5c95fb10
elash1312 <lasherin1312@gmail.com>
2022-04-28 22:49:09 +0000

    installed icons

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Todo from "./todo.jsx";
 import Todoform from "./todoform.jsx";
 
 function Todolist() {
@@ -11,12 +12,15 @@ function Todolist() {
 		const newTodos = [todo, ...todos];
 
 		setTodos(newTodos);
-		console.log(...todos);
 	};
 	return (
 		<div>
 			<h1>What's the Plan for Today?</h1>
 			<Todoform onSubmit={addTodo} />
+			<Todo 
+            todos={todos}
+            completeTodo={completeTodo}
+            />
 		</div>
 	);
 }
