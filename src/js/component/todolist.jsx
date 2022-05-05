61351e428df78cdb7c7cb4f303c43ed138b296ff
elash1312 <lasherin1312@gmail.com>
2022-05-05 05:09:32 +0000

    redid fetch

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -4,6 +4,11 @@ const TodoList = () => {
 	const [todos, setTodos] = React.useState([]);
 	const [task, setTask] = React.useState("");
 
+	const getTodos = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/elash1312")
+		.then(response => response.json)
+		.then(data => { data.label })
+	}
 
 	return (
 		<div>
