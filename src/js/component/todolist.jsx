3b5e4347ef33068a7449f1dd05a05ef6bb32fa85
elash1312 <lasherin1312@gmail.com>
2022-05-06 21:49:21 +0000

    began implementing fetches, still needs work

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const TodoList = () => {
 	const [todos, setTodos] = React.useState([]);
@@ -6,9 +6,59 @@ const TodoList = () => {
 
 	const getTodos = () => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/elash1312")
-		.then(response => response.json)
-		.then(data => { data.label })
-	}
+			.then((response) => response.json())
+			.then((data) => {
+				setTodos(data.map((item) => item.label));
+			});
+	};
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
+	const onKeyPress = (ev) => {
+		if (ev.key === "Enter") {
+			const setter = async () => {
+				setTodos([...todos, task]);
+			};
+			setter();
+			setTask("");
+			updateFetch(todos);
+		}
+	};
+
+	const updateFetch = (newList) => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/elash1312", {
+			method: "PUT",
+			body: JSON.stringify(
+				newList.map((item) => ({ label: item, done: false }))
+			),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+	};
+
+	const deleteTodos = (index) => {
+		const newList = todos.filter((todo, j) => index != j);
+		setTodos(newList);
+		setTask("");
+		updateFetch(newList);
+	};
 
 	return (
 		<div>
@@ -16,10 +66,7 @@ const TodoList = () => {
 				value={task}
 				onChange={(ev) => setTask(ev.currentTarget.value)}
 				onKeyDown={(ev) => {
-					if (ev.key === "Enter") {
-						setTodos([...todos, task]);
-						setTask("");
-					}
+					onKeyPress(ev);
 				}}
 			/>
 			<ul className="ul">
@@ -29,11 +76,7 @@ const TodoList = () => {
 						<button
 							className="x btn btn-warning btn-circle btn-lg text-center font-weight-bold"
 							onClick={() => {
-								const newList = todos.filter(
-									(todo, j) => index != j
-								);
-								setTodos(newList);
-								setTask("");
+								deleteTodos(index);
 							}}>
 							x
 						</button>
